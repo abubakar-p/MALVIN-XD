0d2e77a34863dbb7570130c970124ff75077f713
abubakar-p <abubakarprince0q@gmail.com>
2025-09-01 19:14:18 +0500

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~h6d0TagA#6tt3XCpY4dKia-AIioRa61dTsi2b9yZg-WEJhr2YzUk",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "923244294603", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
